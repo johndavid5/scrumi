582d5811d89bd75f6607ad89287d2d24df1369ea
John D. Aynedjian <johndavid5@yahoo.com>
2017-09-21 22:47:51 -0400

    tweaks and twerks * package.json - update of mongoose, it was hanging during lookup... * templates/objectives.html, do away with limit on name of objective...

--- curl/curl_new_user.bat
+++ curl/curl_new_user.bat
@@ -1,8 +1,9 @@
+REM HINT: try doing db.users.remove({}); for a fresh start...
 REM curl -v --noproxy athena --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -X POST --data "{\"username\":\"arnie\", \"password\":\"pass\"}" http://athena:3000/api/users
-REM curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"arnie\", \"password\":\"pass\"}" http://localhost:3001/api/users
-REM curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"cindy\", \"password\":\"pass\"}" http://localhost:3001/api/users
-curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"kirby\", \"password\":\"pass\"}" http://localhost:3001/api/users
-curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"bennett\", \"password\":\"pass\"}" http://localhost:3001/api/users
-curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"sully\", \"password\":\"pass\"}" http://localhost:3001/api/users
-curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"arias\", \"password\":\"pass\"}" http://localhost:3001/api/users
-curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"cook\", \"password\":\"pass\"}" http://localhost:3001/api/users
+curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"arnie\", \"password\":\"pass\"}" http://localhost:3002/api/users
+curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"cindy\", \"password\":\"pass\"}" http://localhost:3002/api/users
+curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"kirby\", \"password\":\"pass\"}" http://localhost:3002/api/users
+curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"sully\", \"password\":\"pass\"}" http://localhost:3002/api/users
+curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"arias\", \"password\":\"pass\"}" http://localhost:3002/api/users
+curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"cook\", \"password\":\"pass\"}" http://localhost:3002/api/users
+curl -v --noproxy localhost --trace-ascii curl_new_user.trace-ascii.out -H "Content-Type: application/json" -XPOST --data "{\"username\":\"bennett\", \"password\":\"pass\"}" http://localhost:3002/api/users
